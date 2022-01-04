e4d94eed37b4c5dc0f0ed289873a4f8cff49d339
ikawa <pardis367@gmail.com>
2022-01-04 17:55:14 +0330

    hhhaaayyyyyaaaaa(sina)

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
     cout << "this is the second try";
     //gdhhhhhhhhhhhhhhhhhhhkdkkkkkkkkkkkkkkkkk
     cout << "clion is better than visual studio. I hate visual. Clion is the best c IDE";
-
     cout << "kid come down for gods sake";
+    cout << "I say hagh. Kid don't say bullshit. WE have headache."
     return 0;
 }
